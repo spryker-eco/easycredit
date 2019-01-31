28f1f62ddfa9e2e59089c4ca65057ae26ef83b3a
vhrychenko <aptah199494@gmail.com>
2019-01-31 13:17:52 +0200

    Payment method filter test

---
+++ tests/SprykerEcoTest/Zed/Easycredit/EasycreditFilterPaymentMethodTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEcoTest\Zed\Easycredit;
+
+/**
+ * @group SprykerEcoTest
+ * @group Zed
+ * @group Easycredit
+ * @group EasycreditTest
+ * @group EasycreditFilterPaymentMethodTet
+ */
+class EasycreditFilterPaymentMethodTest extends AbstractEasycreditTest
+{
+    /**
+     * @return void
+     */
+    public function testEasycreditMethodIsLeftAfterFilter(): void
+    {
+        $facade = $this->prepareFacade();
+
+        $quoteTransfer = $this->prepareQuoteTransfer();
+        $paymentMethodsTransfer = $this->preparePaymentMethodsTransfer();
+
+        $quoteTransfer->setTotals($this->prepareTotalsTransfer(20000));
+
+        $paymentMethodsTransfer = $facade->filterPaymentMethods($paymentMethodsTransfer, $quoteTransfer);
+
+        $this->assertEquals(count($paymentMethodsTransfer->getMethods()), 1);
+    }
+
+    /**
+     * @return void
+     */
+    public function testEasycreditMethodIsNotLeftAfterFilter(): void
+    {
+        $facade = $this->prepareFacade();
+
+        $quoteTransfer = $this->prepareQuoteTransfer();
+        $paymentMethodsTransfer = $this->preparePaymentMethodsTransfer();
+
+        $quoteTransfer->setTotals($this->prepareTotalsTransfer(200));
+
+        $paymentMethodsTransfer = $facade->filterPaymentMethods($paymentMethodsTransfer, $quoteTransfer);
+
+        $this->assertEquals(count($paymentMethodsTransfer->getMethods()), 0);
+    }
+}
