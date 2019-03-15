ff964b22db1365971416552377b6983a04656234
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2019-03-15 13:55:36 +0100

    MISC fix doc-block

--- src/SprykerEco/Zed/Easycredit/Business/Saver/EasycreditOrderIdentifierSaver.php
+++ src/SprykerEco/Zed/Easycredit/Business/Saver/EasycreditOrderIdentifierSaver.php
@@ -33,9 +33,9 @@ class EasycreditOrderIdentifierSaver implements EasycreditOrderIdentifierSaverIn
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
      * @param \Generated\Shared\Transfer\SaveOrderTransfer $saveOrderTransfer
      *
-     * @return \Generated\Shared\Transfer\PaymentEasycreditOrderIdentifierTransfer|null
+     * @return \Generated\Shared\Transfer\PaymentEasycreditOrderIdentifierTransfer
      */
-    public function saveOrderIdentifier(QuoteTransfer $quoteTransfer, SaveOrderTransfer $saveOrderTransfer): ?PaymentEasycreditOrderIdentifierTransfer
+    public function saveOrderIdentifier(QuoteTransfer $quoteTransfer, SaveOrderTransfer $saveOrderTransfer): PaymentEasycreditOrderIdentifierTransfer
     {
         $paymentEasycreditOrderIdentifierTransfer = new PaymentEasycreditOrderIdentifierTransfer();
 
