1877cf14bc596fe2cd5670f286770fd9bd777232
Kotiuk <andrii.kotiuk@spryker.com>
2020-09-24 13:56:44 +0300

    supesc-135 deprecated ControllerProvider and added Router

---
+++ src/SprykerEco/Yves/Easycredit/Plugin/Router/EasycreditRouteProviderPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Yves\Easycredit\Plugin\Router;
+
+use Spryker\Yves\Router\Plugin\RouteProvider\AbstractRouteProviderPlugin;
+use Spryker\Yves\Router\Route\RouteCollection;
+use Symfony\Component\HttpFoundation\Request;
+
+class EasycreditRouteProviderPlugin extends AbstractRouteProviderPlugin
+{
+    public const ROUTE_EASYCREDIT_SUCCESS_RESPONSE = 'easycredit/success-response';
+
+    /**
+     * Specification:
+     * - Adds Routes to the RouteCollection.
+     *
+     * @api
+     *
+     * @param \Spryker\Yves\Router\Route\RouteCollection $routeCollection
+     *
+     * @return \Spryker\Yves\Router\Route\RouteCollection
+     */
+    public function addRoutes(RouteCollection $routeCollection): RouteCollection
+    {
+        $route = $this->buildRoute('/easycredit/payment/success', 'Easycredit', 'Easycredit', 'successEasycreditResponse');
+        $route = $route->setMethods([Request::METHOD_GET]);
+        $routeCollection->add(static::ROUTE_EASYCREDIT_SUCCESS_RESPONSE, $route);
+
+        return $routeCollection;
+    }
+}
