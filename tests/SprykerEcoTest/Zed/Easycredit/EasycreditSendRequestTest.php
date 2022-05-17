c7ebfce58af79276e93c05224a33464617784c5e
Timo Klingler <timo.klingler@towa.at>
2022-05-17 15:37:02 +0200

    smaller changes 10

--- tests/SprykerEcoTest/Zed/Easycredit/EasycreditSendRequestTest.php
+++ tests/SprykerEcoTest/Zed/Easycredit/EasycreditSendRequestTest.php
@@ -79,7 +79,7 @@ class EasycreditSendRequestTest extends AbstractEasycreditTest
     public function testSendOrderConfirmationRequest(): void
     {
         $quoteTransfer = $this->prepareQuoteTransfer();
-        $idSalesOrder = $this->tester->createOrder($quoteTransfer);
+        $idSalesOrder = $this->tester->createOrder($quoteTransfer, '');
 
         $facade = $this->prepareFacade();
         $responseTransfer = $facade->sendOrderConfirmationRequest($idSalesOrder);
