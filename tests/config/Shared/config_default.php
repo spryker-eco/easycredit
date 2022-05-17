444c84d92175d3c40ac2dcc0938eed1e000ab941
Timo Klingler <timo.klingler@towa.at>
2022-05-17 11:40:53 +0200

    added modules for codeception.yml (6)

--- tests/config/Shared/config_default.php
+++ tests/config/Shared/config_default.php
@@ -18,9 +18,8 @@ $config[KernelConstants::CORE_NAMESPACES] = [
 ];
 $config[PropelConstants::ZED_DB_ENGINE]
     = strtolower(getenv('SPRYKER_DB_ENGINE') ?: '') ?: PropelConfig::DB_ENGINE_MYSQL;
-$config[PropelConstants::ZED_DB_HOST] = getenv('SPRYKER_DB_HOST');
-$config[PropelConstants::ZED_DB_PORT] = getenv('SPRYKER_DB_PORT');
-$config[PropelConstants::ZED_DB_USERNAME] = getenv('SPRYKER_DB_USERNAME');
-$config[PropelConstants::ZED_DB_PASSWORD] = getenv('SPRYKER_DB_PASSWORD');
-$config[PropelConstants::ZED_DB_DATABASE] = getenv('SPRYKER_DB_DATABASE');
-$config[PropelConstants::USE_SUDO_TO_MANAGE_DATABASE] = false;
+$config[PropelConstants::ZED_DB_HOST] = getenv('DATABASE_HOST') ?: '127.0.0.1';
+$config[PropelConstants::ZED_DB_PORT] = getenv('DATABASE_PORT') ?: '3306';
+$config[PropelConstants::ZED_DB_USERNAME] = getenv('DATABASE_USERNAME') ?: 'root';
+$config[PropelConstants::ZED_DB_PASSWORD] = getenv('DATABASE_PASSWORD') ?: 'secret';
+$config[PropelConstants::ZED_DB_DATABASE] = getenv('DATABASE_NAME') ?: 'eu-docker';
